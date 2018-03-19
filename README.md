7908cb8f763e9d4c94843f2d1fe8649c9bedeaca
Karen Almog <karen.almog@coreos.com>
2018-03-19 16:26:35 +0100

    Update README and start_server

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # talk-of-the-week-os-orchestration
 A reveal.js presentation on automating OS VM deployment and configuration
 
+# Submodules
+After cloning, run `git submodule update --init --recursive` to pull all relevant submodules.
 To start the presentation, run `./start_server.sh`
--- start_server.sh
+++ start_server.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-python -m SimpleHTTPServer 8000 &
+python_version=`python -c "import sys;t='{v[0]}'.format(v=list(sys.version_info[:2]));sys.stdout.write(t)";`
+
+
+case $python_version in
+  2) python -m SimpleHTTPServer 8000 & ;;
+  3) python -m http.server 8000 & ;;
+  *) echo "Problem determining the python version. Exiting..." ; exit 1 ;;
+esac
+
 pid=$!
 
 if [ $? == 0 ]; then
